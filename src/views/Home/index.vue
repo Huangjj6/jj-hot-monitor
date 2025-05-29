9984b6298ac0c195eb6944fc512a32190224de6f
HJJ <1011548020@qq.com>
2025-05-29 17:02:16 +0800

    home面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
-import homeProduct from './components/HomeProduct.vue'
+import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,11 @@ import homeProduct from './components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+u件衣服ii纽约给你一个与iyu件衣服ii纽约给你一个与iy
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+和v独有的统一回复图有风度不要他
+  </HomePanel>
 </template>
