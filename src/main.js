503ce450a76a9410213664469ac4f5822b2bae04
HJJ <1011548020@qq.com>
2025-05-31 22:08:37 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
-import '@/styles/common.scss'
-import { getCategory } from '@/apis/testAPI'  // 导入测试API
+import '@/styles/common.scss' // 导入测试API
 
 const app = createApp(App)
 
@@ -13,3 +13,16 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+  useIntersectionObserver(
+    el,
+    ([entry],) => {
+      if (entry.isIntersecting) {
+        el.src = binding.value
+      }
+    },
+  )
+}
+})
