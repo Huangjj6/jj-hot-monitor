e9b0472ff1574485c8ae45825138917e6773ee4f
HJJ <1011548020@qq.com>
2025-06-15 08:13:24 +0800

    二级列表无限加载

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -31,6 +31,16 @@ const getGoodList = async () => {
 onMounted(() => {
   getGoodList()
 })
+
+const disabled = ref(false)
+const load = async () => {
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  goodList.value=[...goodList.value, ...res.result.items]
+  if (res.result.items.length === 0) {
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
@@ -50,7 +60,7 @@ onMounted(() => {
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
-      <div class="body">
+      <div class="body" v-infinite-scroll="load" :infinite-scroll-disabled="disabled">
          <!-- 商品列表-->
          <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
